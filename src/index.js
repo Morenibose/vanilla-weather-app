8fdf2cfda3e26964da2688c0dae70746c80d80ea
Morenibose <109108462+Morenibose@users.noreply.github.com>
2022-08-26 11:14:35 +0100

    Added the background in CSS, updated city in JS

--- src/index.js
+++ src/index.js
@@ -136,4 +136,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Lagos");
